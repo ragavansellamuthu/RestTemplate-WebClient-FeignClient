8944eeaf17c9ebb8c9346d268350e6c3778fe8b4
Ragavan Sellamuthu <155799199+ragavansellamuthu@users.noreply.github.com>
2024-07-31 08:03:28 +0530

    Add files via upload

---
+++ sender-service/src/main/java/com/techietact/tutorial/SenderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techietact.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderServiceApplication.class, args);
+	}
+
+}
